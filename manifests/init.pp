3165dda6e62727187936d564bd97c4882415affb
Alexander Tsirel <noma4i@gmail.com>
2015-01-20 15:02:23 +1100

    some lint fixes and metadata update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class windows_cis() {
-  $gp_utils_folder = "C:\\ProgramData\\gp_utils"
+  $gp_utils_folder = 'C:\\ProgramData\\gp_utils'
   file { $gp_utils_folder:
-    ensure  => directory,
-    recurse => true,
+    ensure             => directory,
+    recurse            => true,
     source_permissions => ignore,
-    source  => 'puppet:///modules/windows_cis/gp_utils'
+    source             => 'puppet:///modules/windows_cis/gp_utils'
   }
 }
